5612889c128368a3664c0a99d3d142b8de643053
utkarsh raj <utkarshraj525@gmail.com>
2025-02-05 23:04:03 +0530

    Update The Navbar and added custom tailwind css

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import Footer from './Components/Footer'
-import Navbar from './Components/Navbar'
+import { Navbar } from './Components/Navbar';
 import Banner from './Pages/Banner'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/wishlist" element={<h1>Wishlist</h1>} />
+        <Route path="/cart" element={<h1>Cart</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
       <Footer />
     </>
   )
--- Client/src/Pages/Banner.jsx
+++ Client/src/Pages/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Banner() {
   return (
-    <div className='h-[30vh] flex justify-center items-center text-4xl'>
+    <div className='h-[30vh] flex justify-center items-center text-4xl bg-primary'>
       <h1 className='text-2xl font-bold'>VendoraX – <i className='font-normal'>A powerful vendor-centric platform</i></h1>
     </div>
   )
